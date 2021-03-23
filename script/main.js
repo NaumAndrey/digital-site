19270051dc3b381aed4ccb00929280f4da1e6f67
andrey <naumov.net@gamil.com>
2021-03-23 10:57:08 +0300

    first commit

---
+++ script/main.js
@@ -0,0 +1,16 @@
+// 'use strict';
+
+(() => {
+
+    const switchDark = document.querySelector('.round');
+    const body = document.querySelector('body');
+    const h1_like_one = document.querySelector('h1-like-one');
+
+    switchDark.addEventListener('click', () => {
+         body.classList.toggle('dark');
+    });
+
+})()
+
+
+//h1-like-one
