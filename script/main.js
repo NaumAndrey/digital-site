8f78539b52a29397fe719b1eac149d4c55c5d374
andrey <naumov.net@gamil.com>
2021-03-25 17:14:04 +0300

    update-background

--- script/main.js
+++ script/main.js
@@ -5,10 +5,12 @@
     const switchDark = document.querySelector('.round');
     const body = document.querySelector('body');
     const h1_like_one = document.querySelector('h1-like-one');
+    const white = document.querySelector('.p-like');
 
     if (switchDark) {
         switchDark.addEventListener('click', () => {
             body.classList.toggle('dark');
+            white.innerHTML = "Black";
        });
     } else {
         console.log('error');
