41c632bb4196da5936f7809379a48a883b833f9a
andrey <naumov.net@gamil.com>
2021-03-31 12:05:21 +0300

    add-active-form

--- script/main.js
+++ script/main.js
@@ -6,6 +6,8 @@
     const body = document.querySelector('body');
     const h1_like_one = document.querySelector('h1-like-one');
     const white = document.querySelector('.p-like');
+    const button_test = document.querySelector('.button_test');
+    const formtest = document.querySelector('.formtest');
 
     if (switchDark) {
         switchDark.addEventListener('click', () => {
@@ -16,6 +18,8 @@
         console.log('error');
     }
 
-    
+    button_test.addEventListener('click' , () => {
+        formtest.classList.toggle('active');
+    });
 
 })()
