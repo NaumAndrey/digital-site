f5fb2376d791ed787c90ccf92ad92ffbbe0b725a
andrey <naumov.net@gamil.com>
2021-04-01 16:00:33 +0300

    change-see-more

--- script/main.js
+++ script/main.js
@@ -1,4 +1,3 @@
-// 'use strict';
 
 (() => {
 
@@ -8,17 +7,20 @@
     const button_test = document.querySelector('.button_test');
     const formtest = document.querySelector('.formtest');
 
+    const picture1 = document.querySelector('.imgTest1000');
+
     if (switchDark) {
         switchDark.addEventListener('click', () => {
             body.classList.toggle('dark');
             white.innerHTML = "Black";
+            picture1.setAttributej('src','img/1500.jpg');
        });
     } else {
         console.log('error');
     }
-
+    
     button_test.addEventListener('click' , () => {
         formtest.classList.toggle('active');
     });
-    
+
 })()
