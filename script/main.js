4a054618ea767478a6744080820f9766007a94a5
andrey <naumov.net@gamil.com>
2021-04-06 17:46:16 +0300

    version--5.1

--- script/main.js
+++ script/main.js
@@ -4,7 +4,7 @@
     const switchDark = document.querySelector('.round');
     const body = document.querySelector('body');
     const white = document.querySelector('.p-like');
-    const button_test = document.querySelector('.button_test');
+    const button_test = document.querySelector('.button_test2');
     const formtest = document.querySelector('.formtest');
 
     const picture1 = document.querySelector('.imgTest1000');
