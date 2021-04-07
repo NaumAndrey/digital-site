b1b868ea37ef0ae792b534826ddb6ae62b3ef3b6
andrey <naumov.net@gamil.com>
2021-04-07 15:08:28 +0300

    version--5.3

--- script/main.js
+++ script/main.js
@@ -31,6 +31,6 @@
 
     menuBtn.addEventListener('click', () => {
         menuBtn.classList.toggle('active');
-        // menu.classList.toggle('active');
+        menu.classList.toggle('active');
     });
 })()
