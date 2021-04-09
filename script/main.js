161efa75c5600d02c6315b6f04ba4f9cebf08c7c
andrey <naumov.net@gamil.com>
2021-04-09 17:12:17 +0300

    version--6.8

--- script/main.js
+++ script/main.js
@@ -4,28 +4,36 @@
     new WOW().init(); // инициализация анимации
 
     const switchDark = document.querySelector('.round');
-    const body = document.querySelector('body');
+
+    const body  = document.querySelector('body');
     const white = document.querySelector('.p-like');
-    const button_test_1 = document.querySelector('.button_test'); 
-    const button_test = document.querySelector('.button_test2');
-    const formtest = document.querySelector('.formtest');
 
-    const picture1 = document.querySelector('.imgTest1000');
+    const button_test_1 = document.querySelector('.button_test');
+    const button_test   = document.querySelector('.button_test2');
 
+    const formtest  = document.querySelector('.formtest');
+    const picture1  = document.querySelector('.imgTest1000');
     const humburger = document.querySelector('.humburger');
-    const humMenu = document.querySelector('.header-heading__heading_test');
+    const humMenu   = document.querySelector('.header-heading__heading_test');
+    
+    const menuBtn = document.querySelector('.menu-btn');
+    const menu    = document.querySelector('.menu');
 
     if (switchDark) {
+
         switchDark.addEventListener('click', () => {
+
             body.classList.toggle('dark');
-            console.log(white.innerText);
+    
             if (white.innerText == "White") {
                 white.innerText = "Black";
             } else {
                 white.innerText = "White";
                 console.log(white);
             }
+
        });
+
     } else {
         console.log('error');
     }
@@ -37,10 +45,6 @@
     button_test.addEventListener('click' , () => {
         formtest.classList.toggle('active');
     });
-
-
-    const menuBtn = document.querySelector('.menu-btn');
-    const menu = document.querySelector('.menu');
 
     menuBtn.addEventListener('click', () => {
         menuBtn.classList.toggle('active');
