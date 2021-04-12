8c6625dc7348bf30cdfb50eeaf62244f050df7fa
andrey <naumov.net@gamil.com>
2021-04-12 14:50:40 +0300

    update-scrollTo-2

--- script/main.js
+++ script/main.js
@@ -51,19 +51,4 @@
         menu.classList.toggle('active');
     });
 
-
-
-    // const anchors = document.querySelector('main');
-
-    // for (let anchor of anchors ) {
-    //     anchor.addEventListener("click", function (event) {
-    //         event.preventDefault();
-    //         const blockID = anchor.getAttribute('href');
-    //         document.querySelector('' + blockID).scrollIntoView({
-    //             behavior: "smooth",
-    //             block: "start"
-    //         })
-    //     });
-    // }
-
 })()
--- script/scroll.js
+++ script/scroll.js
@@ -1,16 +1,18 @@
-// window.onload = function () {
+
+// (() => {
 
 //     let scrolled;
 //     let timer;
+//     let scroll = document.getElementById("main");
 
-//     document.getElementById("main").onclick = function(){
+//     scroll.addEventListener('click' , () => {
 //         scrolled = window.pageYOffset;
 //         scrollToBootom();
-//     }
-
+//     });
+        
 //     function scrollToBootom() {
 //         if (scrolled > 0) {
-//             window.scrollBy(0, scrolled);
+//             window.scrollBy(10, scrolled);
 //             scrolled = scrolled - 50;
 //             timer = setTimeout(scrollToBootom, 50);
 //         } else {
@@ -18,29 +20,26 @@
 //             window.scrollBy(0,0);
 //         }
 //     }
-// }
+
+// })()
 
 
-(() => {
 
-    let scrolled;
-    let timer;
-    let scroll = document.getElementById("main");
 
-    scroll.addEventListener('click' , () => {
-        scrolled = window.pageYOffset;
-        scrollToBootom();
-    });
-        
-    function scrollToBootom() {
-        if (scrolled > 0) {
-            window.scrollBy(10, scrolled);
-            scrolled = scrolled - 50;
-            timer = setTimeout(scrollToBootom, 50);
-        } else {
-            clearTimeout(timer);
-            window.scrollBy(0,0);
-        }
+    let anchor = document.querySelector("[data-main]");
+
+    if (anchor) {
+    anchor.addEventListener("click", function (e) {
+
+            e.preventDefault();
+
+            let blockID = anchor.getAttribute("href").substr(1),
+            offset = document.getElementById(blockID).getBoundingClientRect().top;
+
+            window.scrollBy ({
+                behavior: "smooth",
+                top: offset - 108,
+            });
+
+        });
     }
-
-})()
